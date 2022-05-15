e79cf9e25f5c9ce4b1cc87d2c025a478d78cd7b7
Vader L <96174525+MoiiL@users.noreply.github.com>
2022-05-15 20:56:20 +0530

    Create Python-Weather-Scraping.py

---
+++ Python-Weather-Scraping.py
@@ -0,0 +1,53 @@
+# Weather scraping using Python from BBC weather website
+
+import os
+import requests
+import json
+
+from urllib.parse import urlencode
+
+import numpy as np
+import pandas as pd
+import re
+
+
+test-city = input('Enter a City: ')
+location-url = 'https://locator-service.api.bbci.co.uk/locations?' + urlencode ({
+     'api-key' : 'AGbFAKx58hyjQScCXIYrxuEwJh2W2cmv',
+     's' : test-city ,
+     'stack' : 'aws' ,
+     'locale' : 'en' ,
+     'filter' : 'international' ,
+     'place-types' : 'settlement, airport, district' ,
+     'order' : 'importance' , 
+     'a' : 'true' ,
+     'format' : 'json'
+})
+location-url
+
+result = requests.get(location-url).json()
+result
+
+#printing the location id
+
+result['response']['results']['results'][0]['id']
+
+# A function to output location ID by taking any city name 
+
+def getlocationid(city):
+  city = city.lower()
+  location_url = 'https://locator-service.api.bbci.co.uk/locations?' + urlencode ({
+     'api-key' : 'AGbFAKx58hyjQScCXIYrxuEwJh2W2cmv',
+     's' : test-city ,
+     'stack' : 'aws' ,
+     'locale' : 'en' ,
+     'filter' : 'international' ,
+     'place-types' : 'settlement, airport, district' ,
+     'order' : 'importance' , 
+     'a' : 'true' ,
+     'format' : 'json'
+   })
+  result = requests.get(location-url).json()
+  locationid = result['response']['results']['results'][0]['id']
+  return locationid
+ 
